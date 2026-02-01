c2e568fa0421fb0068818fec0ebabc2b2854c7fa
peace770 <109117451+peace770@users.noreply.github.com>
2026-02-01 15:25:54 +0200

    Update ForInLine.py

--- ForInLine.py
+++ ForInLine.py
@@ -1,12 +1,15 @@
 import sys
+
 def print_map(map, r, c):
     for i in map:
         print(i)
-def check_input(g_list, r_choice, c_choice):
-    for i in range(5):
+        
+def check_input(g_list, r_choice, c_choice, col_limit):
+    tries = 5
+    for i in range(tries):
         try:
             the_input = int(input("choicea colom :"))
-            if str(the_input).isdigit() and 0 < int(the_input) <= 7:
+            if str(the_input).isdigit() and 0 < int(the_input) <= col_limit:
                 if g_list[the_input - 1] >= 0:
                     r_choice, c_choice = g_list[the_input - 1], the_input - 1
                     g_list[the_input - 1] -= 1
@@ -21,13 +24,15 @@ def check_input(g_list, r_choice, c_choice):
                 raise ValueError
         except Exception as e:
             print(f"its {type(e).__name__}")
-            if 4 - i >= 1:
-                print(f"\ntry hagen\tyou have {4 - i} turn's more!")
+            if (tries - 1) - i >= 1:
+                print(f"\ntry hagen\tyou have {(tries - 1) - i} turn's more!")
             else:
-                sys.exit("sory bat uar luz the last chense")
+                exit("sory bat uar luz the last chense")
+                
 def update_map(r_choice, c_choice, my_map, turn):
     my_map[r_choice][c_choice] = turn
     return my_map
+    
 def check_viner(turn, my_map, r, c, r_choice, c_choice):
     count = 0
     for j in range(c):
@@ -46,7 +51,17 @@ def check_viner(turn, my_map, r, c, r_choice, c_choice):
         else:
             count = 0
     count = 0
-    for i in range(-3, 4):
+    
+    #checing diagonal
+    """
+    we search the grid from three (3) lines before the new cell and to three (3) lines after
+    example: for input 5 we search from 2 (5 - 3) to  9 (5 + 4) becuase of n-1
+    this is the same for lines and columns
+    in every iteration of the loop we check that we are within bounds e.g. 0 <= curr_r < r and 0 <= curr_c < c
+    """
+    start = -3
+    end = 4
+    for i in range(start, end):
         curr_r = r_choice + i
         curr_c = c_choice + i
         if 0 <= curr_r < r and 0 <= curr_c < c:
@@ -57,7 +72,7 @@ def check_viner(turn, my_map, r, c, r_choice, c_choice):
             else:
                 count = 0  
     count = 0
-    for i in range(-3, 4):
+    for i in range(start, end):
         curr_r = r_choice + i
         curr_c = c_choice - i
         if 0 <= curr_r < r and 0 <= curr_c < c:
@@ -68,25 +83,24 @@ def check_viner(turn, my_map, r, c, r_choice, c_choice):
             else:
                 count = 0
     return 0
+
 def user_turn(turn, my_map, r, c, g_list, viner):
     r_choice = c_choice = 0
-    r_choice, c_choice, g_list = check_input(g_list, r_choice, c_choice)
+    r_choice, c_choice, g_list = check_input(g_list, r_choice, c_choice, c)
     my_map = update_map(r_choice, c_choice, my_map, turn)
     viner = check_viner(turn, my_map, r, c, r_choice, c_choice)
     return my_map, g_list, viner
-def criat_bord(r, c):
-    bord = [[0 for _ in range(c)] for _ in range(r)]
-    return bord
+
 def main():
     rows = 6
     columns = 7
-    gravity_list = [5, 5, 5, 5, 5, 5, 5]
+    gravity_list = [rows - 1 for _ in range(columns)]
     print(f"-Walckom!-\nTo the 4 in a wat hever a game's")
-    my_map = criat_bord(rows, columns)  # OK.1
+    my_map = [[0 for _ in range(c)] for _ in range(r)]  # OK.1
     print_map(my_map, rows, columns)
     viner = 0
     turn = 2
-    for i in range(42):
+    for i in range(rows * columns):
         if turn % 2 == 0:  # 1
             turn = 1
         else:  # 2
@@ -105,4 +119,5 @@ def main():
         case 2:
             print("2 red is viner!")
 if __name__ == "__main__":
+
     main()
